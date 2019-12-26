23a5261be683dfef4180e48cfe6a4847710b006d
Abayomi <omosehin14@gmail.com>
2019-12-26 06:59:23 +0100

    added userInterface

--- DatingApp/Data/DataContext.cs
+++ DatingApp/Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,6 @@ namespace DatingApp.Data
 
         }
 
-        
+        public DbSet<User> Users { get; set; }
     }
 }
