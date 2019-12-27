b206fda256015fdaf0e41918d9d01254773a7404
Abayomi <omosehin14@gmail.com>
2019-12-27 18:23:49 +0100

    login implemented

---
+++ DatingApp/Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.Dtos
+{
+    public class UserForLoginDto
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+            
+    }
+}
